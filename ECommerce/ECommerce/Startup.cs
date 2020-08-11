2dcb4586d57eac30b6047789d411205713a2070b
Andrew Smith <andrewcs149@gmail.com>
2020-08-11 11:03:42 -0700

    Add db's.

--- ECommerce/ECommerce/Startup.cs
+++ ECommerce/ECommerce/Startup.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Data;
+using ECommerce.Models;
 using ECommerce.Models.Interfaces;
 using ECommerce.Models.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,11 +19,33 @@ namespace ECommerce
 {
     public class Startup
     {
+        public IConfiguration Configuartion { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuartion = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // register dbcontext
+            services.AddDbContext<ProductsDbContext>(options =>
+           {
+               options.UseSqlServer(Configuartion.GetConnectionString("DefaultConnection"));
+           });
+
+            services.AddDbContext<UserDBContext>(options =>
+            {
+                options.UseSqlServer(Configuartion.GetConnectionString("UserConnection"));
+            });
+
+            services.AddIdentity<AppUsers, IdentityRole>()
+                    .AddEntityFrameworkStores<UserDBContext>()
+                    .AddDefaultTokenProviders();
 
             services.AddTransient<IProducts, ProductsRepository>();
         }
