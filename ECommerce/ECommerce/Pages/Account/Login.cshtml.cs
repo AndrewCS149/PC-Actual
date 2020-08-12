d44a3b228756755746534b310a9a0d64e6fea4a8
Robert Carter <racarter1215@gmail.com>
2020-08-11 18:56:19 -0700

    more changes to user story 2

--- ECommerce/ECommerce/Pages/Account/Login.cshtml.cs
+++ ECommerce/ECommerce/Pages/Account/Login.cshtml.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace ECommerce.Pages.Account.Login
 {
     public class LoginModel : PageModel
     {
+        private readonly SignInManager<AppUsers> _signInManager;
+        public LoginModel(SignInManager<AppUsers> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [BindProperty]
+        public LoginViewModel Input { get; set; }
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, false);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");                
+            }
+            return Page();
+        }
+
+        public class LoginViewModel
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
         }
     }
 }
