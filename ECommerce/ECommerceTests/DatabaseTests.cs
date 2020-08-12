f7d45897751708ba92933780d80a44e9aadfe701
Andrew Smith <andrewcs149@gmail.com>
2020-08-11 21:04:43 -0700

    Add test project.

---
+++ ECommerce/ECommerceTests/DatabaseTests.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Data;
+using ECommerce.Models.Interfaces;
+using ECommerce.Models.Services;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceTests
+{
+    public class DatabaseTests : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        private readonly IProducts _products;
+        private readonly StoreDbContext _StoreDb;
+
+        public DatabaseTests()
+        {
+            _connection = new SqliteConnection("Filename=:memory");
+            _connection.Open();
+            _products = new InventoryManagement()
+        }
+    }
+}
---
+++ ECommerce/ECommerceTests/InventoryManagementTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ECommerceTests
+{
+    public class InventoryManagementTests
+    {
+        [Fact]
+        public void CanGetAllItems()
+        {
+        }
+    }
+}
