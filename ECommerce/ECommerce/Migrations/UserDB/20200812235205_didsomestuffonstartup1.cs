774eaeb2fdf083bc472f8deb52e45e01da5a976f
Robert Carter <racarter1215@gmail.com>
2020-08-12 17:18:23 -0700

    commit for user story 4

---
+++ ECommerce/ECommerce/Migrations/UserDB/20200812235205_didsomestuffonstartup1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerce.Migrations.UserDB
+{
+    public partial class didsomestuffonstartup1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ECommerce/ECommerce/Models/RoleInitializer.cs
@@ -0,0 +1,53 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    public class RoleInitializer
+    {
+        // Create a list of identity roles
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+        {
+            new IdentityRole{Name = AppRoles.Admin, NormalizedName = AppRoles.Admin.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString()},
+            new IdentityRole{Name = AppRoles.User, NormalizedName = AppRoles.User.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString()},
+        };
+        public static void SeedData(IServiceProvider serviceProvider, UserManager<AppUsers> userManager, IConfiguration _config)
+        {
+            using (var dbContext = new UserDBContext(serviceProvider.GetRequiredService<DbContextOptions<UserDBContext>>()))
+            {
+                dbContext.Database.EnsureCreated();
+                AddRoles(dbContext);
+                SeedUsers(userManager, _config);
+            }
+        }
+        public static void AddRoles(UserDBContext dbContext)
+        {
+            if (dbContext.Roles.Any()) return;
+            foreach (var role in Roles)
+            {
+                dbContext.Roles.Add(role);
+                dbContext.SaveChanges();
+            }
+        }
+        public static void SeedUsers(UserManager<AppUsers> userManager, IConfiguration _config)
+        {
+            if (userManager.FindByNameAsync(_config["AdminEmail"]).Result == null)
+            {
+                AppUsers user = new AppUsers();
+                user.Email = _config["AdminEmail"];
+                IdentityResult result = userManager.CreateAsync(user, _config["AdminPassword"]).Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, AppRoles.Admin);
+                }
+            }
+        }
+    }
+}
