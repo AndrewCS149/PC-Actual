171a4a1eca86b9afd4d4779f0ea82c6effc49f7a
Andrew Smith <andrewcs149@gmail.com>
2020-08-23 12:07:48 -0700

    Add order model.

---
+++ ECommerce/ECommerce/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    public class Order
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
